938f21c8a13286467327bfec2b6c281374b90c25
VanFossen <nvanfossen@gmail.com>
2022-07-18 17:35:41 -0400

    working!!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Dialog } from "@mui/material";
 
-import { ReactComponent as Main } from "./main.svg";
+import { ReactComponent as Main } from "./components/svgs/ftir-main.svg";
 import "./App.css";
+import { imgSource, toolTips } from "./dictionaries/SVGLibrary";
 
 export default function App() {
+  const [toggled, setToggled] = useState(false);
+  const [element, setElement] = useState(null);
+
   const handleClick = (event) => {
-    console.log(event.target.parentElement.id);
+    setToggled(!toggled);
+
+    if (event.target.parentElement.id === "app") {
+      setElement(null);
+    } else {
+      setElement(event.target.parentElement.id);
+    }
   };
 
   return (
     <div id="app">
       <Main id="main" onClick={handleClick} />
+
+      {element && (
+        <Dialog className="popup" onClose={handleClick} open={toggled}>
+          <h2>{toolTips[element].title}</h2>
+          <img src={imgSource[element]} className="example-image" alt="" />
+          <p>{toolTips[element].text}</p>
+        </Dialog>
+      )}
     </div>
   );
 }
---
+++ src/dictionaries/SVGLibrary.js
@@ -0,0 +1,54 @@
+import wheel from "../components/svgs/tooltip/aperture-wheel.svg";
+import fcc from "../components/svgs/tooltip/fixed-corner-cube.svg";
+import fm from "../components/svgs/tooltip/fixed-mirror.svg";
+import frm from "../components/svgs/tooltip/flat-rotatable-mirror-1.svg";
+import dcfrm from "../components/svgs/tooltip/flat-rotatable-mirror-2.svg";
+import cdfrm from "../components/svgs/tooltip/flat-rotatable-mirror-3.svg";
+import globar from "../components/svgs/tooltip/globar.svg";
+import laser from "../components/svgs/tooltip/laser.svg";
+import lnsb from "../components/svgs/tooltip/lnsb.svg";
+import mct from "../components/svgs/tooltip/mct.svg";
+import mcc from "../components/svgs/tooltip/movable-corner-cube.svg";
+import pm from "../components/svgs/tooltip/parabolic-mirror.svg";
+import pmh1 from "../components/svgs/tooltip/parabolic-mirror-hole-1.svg";
+import pmh2 from "../components/svgs/tooltip/parabolic-mirror-hole-2.svg";
+import sample from "../components/svgs/tooltip/sample-compartment.svg";
+import tungsten from "../components/svgs/tooltip/tungsten.svg";
+
+export const imgSource = {
+  "aperture-wheel": wheel,
+  "fixed-corner-cube": fcc,
+  "fixed-mirror": fm,
+  "flat-rotatable-mirror-1": frm,
+  "flat-rotatable-mirror-2": dcfrm,
+  "flat-rotatable-mirror-3": cdfrm,
+  globar: globar,
+  laser: laser,
+  lnsb: lnsb,
+  mct: mct,
+  "movable-corner-cube": mcc,
+  "parabolic-mirror": pm,
+  "parabolic-mirror-hole-1": pmh1,
+  "parabolic-mirror-hole-2": pmh2,
+  "sample-compartment": sample,
+  tungsten: tungsten,
+};
+
+export const toolTips = {
+  "aperture-wheel": { title: "Aperture Wheel", text: "" },
+  "fixed-corner-cube": { title: "Fixed Corner Cube", text: "" },
+  "fixed-mirror": { title: "Fixed Mirror", text: "" },
+  "flat-rotatable-mirror-1": { title: "Flat Rotatable Mirror", text: "" },
+  "flat-rotatable-mirror-2": { title: "Flat Rotatable Mirror", text: "" },
+  "flat-rotatable-mirror-3": { title: "Flat Rotatable Mirror", text: "" },
+  globar: { title: "Globar", text: "" },
+  laser: { title: "Laser", text: "" },
+  lnsb: { title: "LNSB", text: "" },
+  mct: { title: "MCT", text: "" },
+  "movable-corner-cube": { title: "Movable Corner Cube", text: "" },
+  "parabolic-mirror": { title: "Parabolic Mirror", text: "" },
+  "parabolic-mirror-hole-1": { title: "Parabolic Mirror With Hole", text: "" },
+  "parabolic-mirror-hole-2": { title: "Parabolic Mirror With Hole", text: "" },
+  "sample-compartment": { title: "Sample Compartment", text: "" },
+  tungsten: { title: "Tungsten", text: "" },
+};
